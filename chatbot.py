08e240defedc05f90e49d5a0b0d7cb9dbf40369e
MustafaAkolawala <mustafaakolawala@eng.rizvi.edu.in>
2023-09-03 00:08:19 +0530

    Process data from Json file
    tokenized and stemmed the data from the json file and segregated
    them into separate lists for creating the training data accordingly.

---
+++ chatbot.py
@@ -0,0 +1,36 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+import numpy
+import tensorflow
+import random
+import json
+
+stemmer = LancasterStemmer()
+
+with open("intent.json") as file:
+    data = json.load(file)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+
+        docs_y.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+
+
+    
